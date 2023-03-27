5c8be4501bf7aeabbc21ad2da47f6bbe159d4255
Muhammad Umair Yaqub <umairyaqubofficial@gmail.com>
2023-03-27 20:36:13 +0500

    Modernization Update

--- replace_domain.py
+++ replace_domain.py
@@ -1,7 +1,9 @@
 def replace_domain(email, old_domain, new_domain):
-   if "@" + old_domain in email:
-           index = email.index("@" + old_domain)
-           new_email = email[:index] + "@" + new_domain
-           return new_email
-   return email
-replace_domain ("abc@yahoo.com", "yahoo.com", "gmail.com")
+    if "@" + old_domain in email:
+        index = email.index("@" + old_domain)
+        new_email = email[:index] + "@" + new_domain
+        return new_email
+    return email
+
+
+replace_domain("abc@yahoo.com", "yahoo.com", "gmail.com")
